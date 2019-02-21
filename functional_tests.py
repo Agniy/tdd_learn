209b8b94e6b1f708c4aef1060329569be74aa7f1
Ilya <sunycatmsk@gmail.com>
2019-02-21 09:44:35 +0300

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -41,14 +41,15 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
         #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
         # )
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('2: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
