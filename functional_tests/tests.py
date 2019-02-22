facc7e2589e0b33cf9e0bdf87274af1ba0f7e3f2
Ilya <sunycatmsk@gmail.com>
2019-02-22 16:22:05 +0300

    Тест ожидания появления элемента на странице.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+MAX_WAIT = 10
 #class NewVisitorTest(unittest.TestCase):  #данные будут создаваться в реальной базе
 class NewVisitorsTest(LiveServerTestCase): #данные будут создаваться в тестовой базе
     def setUp(self):
@@ -16,6 +18,21 @@ class NewVisitorsTest(LiveServerTestCase): #данные будут создав
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -42,7 +59,14 @@ class NewVisitorsTest(LiveServerTestCase): #данные будут создав
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # table = self.browser.find_element_by_id('id_list_table')
         # rows = table.find_elements_by_tag_name('tr')
@@ -50,8 +74,8 @@ class NewVisitorsTest(LiveServerTestCase): #данные будут создав
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
         #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
         # )
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        #self.check_for_row_in_list_table('2: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
